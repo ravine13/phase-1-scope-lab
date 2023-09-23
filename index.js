520621474b1d2caac583d008cd1ab7d33f173414
ravine13 <kuriaderek@gmail.com>
2023-09-23 21:00:41 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+customerName = `bob`
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+//upperCaseCustomerName();
+//console.log(customerName);
+
+
+// console.log(bestCustomer)
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+//setBestCustomer();
+//nsole.log(bestCustomer)
+
+function overwriteBestCustomer(X){
+    bestCustomer = X
+}
+//overwriteBestCustomer('maybe bob')
+//console.log(bestCustomer)
+const leastFavoriteCustomer = "X"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "y"
+}
